c79b20b2cc7346d9358f204f266c1a63bc60452e
Fabian <garzafabain204@gmail.com>
2023-11-29 16:31:54 -0600

    Added front end code

--- app.py
+++ app.py
@@ -1,2 +1,24 @@
 #First Commit
 print("Hello World")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+app.run()
+
+# Import libraries and packages, they contain additional functions.
+from flask import Flask, render_template
+# Create web application using Flask
+app = Flask(__name__)
+# Create our routes
+@app.route('/')
+def home():
+ return render_template('index.html')
+# Start the web application
+app.run(
+ debug=True
+)
