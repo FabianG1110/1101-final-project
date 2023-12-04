db522d94c2bbf31696baf01cffef78a84cb0cf38
Fabian <garzafabain204@gmail.com>
2023-12-04 16:40:02 -0600

    Updated Website

--- app.py
+++ app.py
@@ -1,15 +1,3 @@
-#First Commit
-print("Hello World")
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-app.run()
-
 # Import libraries and packages, they contain additional functions.
 from flask import Flask, render_template
 # Create web application using Flask
@@ -19,6 +7,3 @@ app = Flask(__name__)
 def home():
  return render_template('index.html')
 # Start the web application
-app.run(
- debug=True
-)
